afa690cdf17802aef198b4544e4890a64d1e6990
Igor Chepurnoi <chepurnoi.igor@gmail.com>
2020-03-21 22:23:49 +0200

    Init package

---
+++ src/Exceptions/InvalidConfigException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Clewo\Fixture\Exceptions;
+
+use Exception;
+
+/**
+ * Class InvalidConfigException represents an exception caused by incorrect object configuration.
+ */
+class InvalidConfigException extends Exception
+{
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName()
+    {
+        return 'Invalid Configuration';
+    }
+}
